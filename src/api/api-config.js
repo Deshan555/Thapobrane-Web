161c8ee47c12f91cf3e275eb4172693d8d9159af
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-02-02 22:46:38 +0530

    Add base API configuration details

---
+++ src/api/api-config.js
@@ -0,0 +1,8 @@
+const baseDetails = {
+    BASE_URL: 'http://localhost:3001/',  // Added 'http://'
+    CORE_SERVICE_URL: 'http://localhost:3001/thaprobane/core/v01',  // Added 'http://'
+    STORAGE_SERVICE_URL: 'https://api.themoviedb.org/3',
+    GATEWAY_SERVICE_URL: 'https://api.themoviedb.org/3',
+};
+
+export { baseDetails };
