03ef0054605cf46689f6286381eec325ce3087c4
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-02-08 22:46:43 +0530

    Add Exports component

---
+++ src/pages/vehicleModule/Exports.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+const Exports = () => {
+    return (
+        <>
+        <h1>
+            Exports
+        </h1>
+      </>
+    )
+}
+
+export default Exports;
