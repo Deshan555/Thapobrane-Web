5bdd4a8e8526685703e2fbca36dca4d0397a714c
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-02-08 22:46:06 +0530

    Add EnvironmentalZones component

---
+++ src/pages/vehicleModule/EnvironmentalZones.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+const EnvironmentalZones = () => {
+    return (
+        <>
+        <h1>
+            Environmental Zones
+        </h1>
+      </>
+    )
+}
+
+export default EnvironmentalZones;
