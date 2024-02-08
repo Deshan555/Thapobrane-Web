8babe30edae15c853aa75a54f1953bed2ec06030
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-02-08 22:43:42 +0530

    Add DataImporter component

---
+++ src/pages/vehicleModule/DataImporter.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+const DataImporter = () => {
+    return (
+        <>
+        <h1>
+            Data Importer
+        </h1>
+      </>
+    )
+}
+
+export default DataImporter;
