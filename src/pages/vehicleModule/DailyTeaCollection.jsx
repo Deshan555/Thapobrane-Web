0b761dc02ec742cf9226900e2d66bcf09809cce5
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-02-08 22:43:32 +0530

    Add DailyTeaCollection component

---
+++ src/pages/vehicleModule/DailyTeaCollection.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+const DailyTeaCollection = () => {
+    return (
+        <>
+        <h1>
+            Daily Tea Collection
+        </h1>
+      </>
+    )
+}
+
+export default DailyTeaCollection;
