29cf0d280bb947ee390aca0958a0cc3bc5eedd2f
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-02-08 22:45:58 +0530

    Add MasterData and VersionsInfo components

---
+++ src/pages/vehicleModule/VersionsInfo.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+const VersionsInfo = () => {
+    return (
+        <>
+        <h1>
+            Versions Info
+        </h1>
+      </>
+    )
+}
+
+export default VersionsInfo;
