e21d2bee60a1c701d5cfc78aa59168254d780b36
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-02-08 22:36:08 +0530

    Add Customers component

---
+++ src/pages/vehicleModule/Customers.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+const Customers = () => {
+    return (
+        <>
+        <h1>
+            Customers
+        </h1>
+      </>
+    )
+}
+
+export default Customers;
