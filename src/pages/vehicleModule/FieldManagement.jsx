f37110281fe89321412b2e6ee0b7ebf93949736a
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-02-08 22:45:33 +0530

    Add FieldManagement component

---
+++ src/pages/vehicleModule/FieldManagement.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+const FieldManagement = () => {
+    return (
+        <>
+        <h1>
+            Field Management
+        </h1>
+      </>
+    )
+}
+
+export default FieldManagement;
