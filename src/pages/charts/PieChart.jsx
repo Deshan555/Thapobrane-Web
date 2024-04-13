44683ff035dbb786f465f0bee79412f8383386cf
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-04-13 12:47:23 +0530

    Add PieChart component to display data in a pie chart

---
+++ src/pages/charts/PieChart.jsx
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import Chart from 'react-apexcharts';
+import './chart-styles.css'
+
+class PieChart extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            options: {
+                labels: props?.categories,
+                legend: {
+                    show: true,
+                    showForSingleSeries: false,
+                    showForNullSeries: true,
+                    showForZeroSeries: true,
+                    position: 'bottom',
+                    horizontalAlign: 'center', 
+                    floating: false,
+                    fontSize: '12px',
+                    fontFamily: 'Poppins, sans-serif',
+                    fontWeight: 400,
+                    formatter: undefined,
+                    inverseOrder: false,
+                    width: undefined,
+                    height: undefined,
+                    tooltipHoverFormatter: undefined,
+                    customLegendItems: [],
+                    offsetX: 0,
+                    offsetY: 0,
+                    labels: {
+                        colors: undefined,
+                        useSeriesColors: false
+                    },
+                    markers: {
+                        width: 12,
+                        height: 12,
+                        strokeWidth: 0,
+                        strokeColor: '#fff',
+                        fillColors: undefined,
+                        radius: 12,
+                        customHTML: undefined,
+                        onClick: undefined,
+                        offsetX: 0,
+                        offsetY: 0
+                    },
+                    itemMargin: {
+                        horizontal: 5,
+                        vertical: 0
+                    },
+                    onItemClick: {
+                        toggleDataSeries: true
+                    },
+                    onItemHover: {
+                        highlightDataSeries: true
+                    },
+                },              
+                responsive: [{
+                    breakpoint: 480,
+                    options: {
+                      chart: {
+                        width: 300
+                      },
+                      legend: {
+                        show: false,
+                        position: 'bottom'
+                      }
+                    }
+                  }]
+            },
+            toolbar: {
+                show: true,
+                tools: {
+                  download: true,
+                  selection: true,
+                  zoom: true,
+                  zoomin: true,
+                  zoomout: true,
+                  pan: true,
+                  reset: true,
+                  customIcons: []
+                },
+                autoSelected: 'zoom' 
+            },
+            series: props?.data,
+        }
+    }
+    render(){
+        return(
+            <div className="pie-chart">
+                <Chart
+                    style={{margin: 'auto', backgroundColor: 'white'}}
+                    options={this.state.options}
+                    series={this.state.series}
+                    type="pie"
+                    height={480}
+                />
+            </div>
+        );
+    }
+}
+
+export default PieChart;
