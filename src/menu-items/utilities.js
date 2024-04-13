8d744e76008b33f92290c8e521ccb2c24600cced
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-04-13 15:04:11 +0530

    Update menu items in utilities.js to include data exports and data importer

--- src/menu-items/utilities.js
+++ src/menu-items/utilities.js
@@ -1,23 +1,25 @@
 // assets
-import {
-  AppstoreAddOutlined,
-  AntDesignOutlined,
-  BarcodeOutlined,
-  BgColorsOutlined,
-  FontSizeOutlined,
-  LoadingOutlined
-} from '@ant-design/icons';
+import { LoginOutlined, ProfileOutlined, CarOutlined, FieldTimeOutlined, CodeOutlined, CloudDownloadOutlined, CloudUploadOutlined, ExperimentOutlined, AppstoreAddOutlined, TrophyOutlined, UserOutlined, TeamOutlined, SolutionOutlined, NodeIndexOutlined, NumberOutlined, EnvironmentOutlined } from '@ant-design/icons';
 
 // icons
 const icons = {
-  FontSizeOutlined,
-  BgColorsOutlined,
-  BarcodeOutlined,
-  AntDesignOutlined,
-  LoadingOutlined,
-  AppstoreAddOutlined
+  LoginOutlined,
+  ProfileOutlined,
+  CarOutlined,
+  FieldTimeOutlined,
+  TrophyOutlined,
+  UserOutlined,
+  TeamOutlined,
+  SolutionOutlined,
+  NodeIndexOutlined,
+  NumberOutlined,
+  ExperimentOutlined,
+  EnvironmentOutlined,
+  AppstoreAddOutlined, 
+  CloudDownloadOutlined, 
+  CloudUploadOutlined,
+  CodeOutlined
 };
-
 // ==============================|| MENU ITEMS - UTILITIES ||============================== //
 
 const utilities = {
@@ -25,42 +27,69 @@ const utilities = {
   title: 'Utilities',
   type: 'group',
   children: [
+    // {
+    //   id: 'util-typography',
+    //   title: 'Typography',
+    //   type: 'item',
+    //   url: '/typography',
+    //   icon: icons.FontSizeOutlined
+    // },
+    // {
+    //   id: 'util-color',
+    //   title: 'Color',
+    //   type: 'item',
+    //   url: '/color',
+    //   icon: icons.BgColorsOutlined
+    // },
+    // {
+    //   id: 'util-shadow',
+    //   title: 'Shadow',
+    //   type: 'item',
+    //   url: '/shadow',
+    //   icon: icons.BarcodeOutlined
+    // },
+    // {
+    //   id: 'ant-icons',
+    //   title: 'Ant Icons',
+    //   type: 'item',
+    //   url: '/icons/ant',
+    //   icon: icons.AntDesignOutlined,
+    //   breadcrumbs: false
+    // },
+    // {
+    //   id : 'loading-icons',
+    //   title: 'Loading Icons',
+    //   type: 'item',
+    //   url: '/icons/loading',
+    //   icon: icons.LoadingOutlined,
+    //   breadcrumbs: true
+    // }
     {
-      id: 'util-typography',
-      title: 'Typography',
+      id: 'exports',
+      title: 'Data Exports',
       type: 'item',
-      url: '/typography',
-      icon: icons.FontSizeOutlined
-    },
-    {
-      id: 'util-color',
-      title: 'Color',
-      type: 'item',
-      url: '/color',
-      icon: icons.BgColorsOutlined
-    },
-    {
-      id: 'util-shadow',
-      title: 'Shadow',
-      type: 'item',
-      url: '/shadow',
-      icon: icons.BarcodeOutlined
-    },
-    {
-      id: 'ant-icons',
-      title: 'Ant Icons',
-      type: 'item',
-      url: '/icons/ant',
-      icon: icons.AntDesignOutlined,
+      url: '/thaprobane/exports',
+      icon: icons.CloudDownloadOutlined,
+      target: false,
       breadcrumbs: false
     },
     {
-      id : 'loading-icons',
-      title: 'Loading Icons',
+      id: 'dataImporter',
+      title: 'Data Importer',
       type: 'item',
-      url: '/icons/loading',
-      icon: icons.LoadingOutlined,
-      breadcrumbs: true
+      url: '/thaprobane/data-importer',
+      icon: icons.CloudUploadOutlined,
+      target: false,
+      breadcrumbs: false
+    },
+    {
+      id: 'versionsInfo',
+      title: 'Versions Info',
+      type: 'item',
+      url: '/thaprobane/versions-info',
+      icon: icons.CodeOutlined,
+      target: false,
+      breadcrumbs: false
     }
   ]
 };
