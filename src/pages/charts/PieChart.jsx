6b2170406686b54fbc18a1e1a54fa5b001036874
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-04-18 00:38:15 +0530

    Update PieChart component to include props for labels and legend options

--- src/pages/charts/PieChart.jsx
+++ src/pages/charts/PieChart.jsx
@@ -5,83 +5,84 @@ import './chart-styles.css'
 class PieChart extends Component{
     constructor(props){
         super(props);
+        const t = this.props.data;
         this.state = {
             options: {
-                labels: props?.categories,
-                legend: {
-                    show: true,
-                    showForSingleSeries: false,
-                    showForNullSeries: true,
-                    showForZeroSeries: true,
-                    position: 'bottom',
-                    horizontalAlign: 'center', 
-                    floating: false,
-                    fontSize: '12px',
-                    fontFamily: 'Poppins, sans-serif',
-                    fontWeight: 400,
-                    formatter: undefined,
-                    inverseOrder: false,
-                    width: undefined,
-                    height: undefined,
-                    tooltipHoverFormatter: undefined,
-                    customLegendItems: [],
-                    offsetX: 0,
-                    offsetY: 0,
-                    labels: {
-                        colors: undefined,
-                        useSeriesColors: false
-                    },
-                    markers: {
-                        width: 12,
-                        height: 12,
-                        strokeWidth: 0,
-                        strokeColor: '#fff',
-                        fillColors: undefined,
-                        radius: 12,
-                        customHTML: undefined,
-                        onClick: undefined,
-                        offsetX: 0,
-                        offsetY: 0
-                    },
-                    itemMargin: {
-                        horizontal: 5,
-                        vertical: 0
-                    },
-                    onItemClick: {
-                        toggleDataSeries: true
-                    },
-                    onItemHover: {
-                        highlightDataSeries: true
-                    },
-                },              
-                responsive: [{
-                    breakpoint: 480,
-                    options: {
-                      chart: {
-                        width: 300
-                      },
-                      legend: {
-                        show: false,
-                        position: 'bottom'
-                      }
-                    }
-                  }]
+            labels: t[0] ? props?.categories : [],
+            legend: {
+                show: true,
+                showForSingleSeries: false,
+                showForNullSeries: true,
+                showForZeroSeries: true,
+                position: 'bottom',
+                horizontalAlign: 'center', 
+                floating: false,
+                fontSize: '12px',
+                fontFamily: 'Poppins, sans-serif',
+                fontWeight: 400,
+                formatter: undefined,
+                inverseOrder: false,
+                width: undefined,
+                height: undefined,
+                tooltipHoverFormatter: undefined,
+                customLegendItems: [],
+                offsetX: 0,
+                offsetY: 0,
+                labels: {
+                colors: undefined,
+                useSeriesColors: false
+                },
+                markers: {
+                width: 12,
+                height: 12,
+                strokeWidth: 0,
+                strokeColor: '#fff',
+                fillColors: undefined,
+                radius: 12,
+                customHTML: undefined,
+                onClick: undefined,
+                offsetX: 0,
+                offsetY: 0
+                },
+                itemMargin: {
+                horizontal: 5,
+                vertical: 0
+                },
+                onItemClick: {
+                toggleDataSeries: true
+                },
+                onItemHover: {
+                highlightDataSeries: true
+                },
+            },              
+            responsive: [{
+                breakpoint: 480,
+                options: {
+                  chart: {
+                width: 300
+                  },
+                  legend: {
+                show: false,
+                position: 'bottom'
+                  }
+                }
+              }]
             },
             toolbar: {
-                show: true,
-                tools: {
-                  download: true,
-                  selection: true,
-                  zoom: true,
-                  zoomin: true,
-                  zoomout: true,
-                  pan: true,
-                  reset: true,
-                  customIcons: []
-                },
-                autoSelected: 'zoom' 
+            show: true,
+            tools: {
+              download: true,
+              selection: true,
+              zoom: true,
+              zoomin: true,
+              zoomout: true,
+              pan: true,
+              reset: true,
+              customIcons: []
             },
-            series: props?.data,
+            autoSelected: 'zoom' 
+            },
+            series: t,
         }
     }
     render(){
