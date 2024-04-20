a9e46c0133954a7efba30880a15fe9ab9eef0f2f
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-04-20 12:02:32 +0530

    Update Login page heading style and remove commented code in Login.js

--- src/pages/authentication/Login.js
+++ src/pages/authentication/Login.js
@@ -15,7 +15,7 @@ const Login = () => (
     <Grid container spacing={3}>
       <Grid item xs={12}>
         <Stack direction="row" justifyContent="space-between" alignItems="baseline" sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
-          <Typography variant="h3" className="headingStyle">
+          <Typography className="headingStyle-lite">
             Admin LogIn
           </Typography>
           <Typography component={Link} to="/register" 
@@ -23,7 +23,7 @@ const Login = () => (
           sx={{ textDecoration: 'none' }} 
           color="primary"
           className='textStyles-small'>
-            Don&apos;t have an account?
+            {/* Don&apos;t have an account? */}
           </Typography>
         </Stack>
       </Grid>
