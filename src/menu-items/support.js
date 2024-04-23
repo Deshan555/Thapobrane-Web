a626c11d9e2fad6ef00f2a59212dc3e181a766d6
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-04-23 09:54:32 +0530

    Add support menu item and icons to the admin dashboard

--- src/menu-items/support.js
+++ src/menu-items/support.js
@@ -1,36 +1,90 @@
 // assets
-import { ChromeOutlined, QuestionOutlined } from '@ant-design/icons';
+import { ChromeOutlined, QuestionOutlined, SettingOutlined, UserSwitchOutlined, SafetyCertificateOutlined, SolutionOutlined } from '@ant-design/icons';
 
 // icons
 const icons = {
   ChromeOutlined,
-  QuestionOutlined
+  QuestionOutlined,
+  UserSwitchOutlined,
+  SettingOutlined,
+  SafetyCertificateOutlined,
+  SolutionOutlined
 };
 
 // ==============================|| MENU ITEMS - SAMPLE PAGE & DOCUMENTATION ||============================== //
 
-const support = {
+const adminPages = {
   id: 'support',
   title: 'Support',
   type: 'group',
   children: [
+    // {
+    //   id: 'sample-page',
+    //   title: 'Sample Page',
+    //   type: 'item',
+    //   url: '/sample-page',
+    //   icon: icons.ChromeOutlined
+    // },
     {
-      id: 'sample-page',
-      title: 'Sample Page',
+      id: 'profile',
+      title: 'Profile Settings',
       type: 'item',
-      url: '/sample-page',
-      icon: icons.ChromeOutlined
+      url: '/thaprobane/profile',
+      icon: icons.SettingOutlined,
+      target: false,
+      breadcrumbs: false
+    },
+    {
+      id: 'complaints',
+      title: 'Complaints',
+      type: 'item',
+      url: '/thaprobane/complaints',
+      icon: icons.SolutionOutlined,
+      target: false,
+      breadcrumbs: false
+    },
+    {
+      id: 'privacy',
+      title: 'Privacy',
+      type: 'item',
+      url: '/thaprobane/privacy',
+      icon: icons.SafetyCertificateOutlined,
+      target: false,
+      breadcrumbs: false
     },
     {
       id: 'documentation',
-      title: 'Documentation',
+      title: 'Development',
       type: 'item',
       url: 'https://codedthemes.gitbook.io/mantis/',
       icon: icons.QuestionOutlined,
       external: true,
       target: true
-    }
+    },
   ]
 };
+
+const nonAdminPages = [
+  {
+    id: 'profile-customer',
+    title: 'Profile Settings',
+    type: 'item',
+    url: ' /thaprobane/customer-profile',
+    icon: icons.SettingOutlined,
+    target: false,
+    breadcrumbs: false
+  },
+];
+
+const support = {
+id: 'app-support',
+title: 'Support',
+type: 'group',
+children: localStorage?.getItem('userRole') === "ROLE.CUSTOMER" ? nonAdminPages : adminPages.children
+};
+
+
+
+
 
 export default support;
