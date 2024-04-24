29accc53d974b8da593040d2ab1f63c7fca42903
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-04-24 22:17:11 +0530

    Add authentication check logic in AuthChecker.js

---
+++ src/pages/vehicleModule/AuthChecker.js
@@ -0,0 +1,22 @@
+export const authenticationCheck = async (navigate) => {
+    
+  const accessTokenExpireDate = localStorage.getItem('atokenExpireDate'); 
+  const refreshTokenExpireDate = localStorage.getItem('rtokenExpireDate');
+  const userRole = localStorage.getItem('userRole');
+  const accessToken = localStorage.getItem('atoken');
+  const refreshToken = localStorage.getItem('rtoken');
+
+  if (accessTokenExpireDate !== null && refreshTokenExpireDate !== null && userRole !== null && accessToken !== null && refreshToken !== null) {
+    const currentDate = new Date();
+    const accessTokenExpire = new Date(accessTokenExpireDate);
+    const refreshTokenExpire = new Date(refreshTokenExpireDate);
+
+    if (currentDate < accessTokenExpire && currentDate < refreshTokenExpire) {
+      console.log('User Authenticated');
+    } else {
+      navigate('/login');
+    }
+  } else {
+    navigate('/login');
+  }
+}
