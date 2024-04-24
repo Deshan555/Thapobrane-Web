876f885bfd1b05b93b3abc3fdbca7e8e4d7ad077
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-04-24 22:18:51 +0530

    add sample jsx files for add more options => need to development

---
+++ src/pages/vehicleModule/AddComplains.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+const AddComplains = () => {
+    return (
+        <>
+        <h1>
+            Master Data
+        </h1>
+      </>
+    )
+}
+
+export default AddComplains;
---
+++ src/pages/vehicleModule/Privacy.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+const Privacy = () => {
+    return (
+        <>
+        <h1>
+            Master Data
+        </h1>
+      </>
+    )
+}
+
+export default Privacy;
