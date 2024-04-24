ca70f6d93371a2ca1211d6a0b22043783b135a19
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-04-24 22:10:09 +0530

    Refactor chart styles and add support for custom fonts in chart-styles.css

--- src/pages/charts/DualBarChart.jsx
+++ src/pages/charts/DualBarChart.jsx
@@ -1,183 +1,7 @@
-     
-        // var options = {
-        //     series: [{
-        //     data: [44, 55, 41, 64, 22, 43, 21]
-        //   }, {
-        //     data: [53, 32, 33, 52, 13, 44, 32]
-        //   }],
-        //     chart: {
-        //     type: 'bar',
-        //     height: 430
-        //   },
-        //   plotOptions: {
-        //     bar: {
-        //       horizontal: true,
-        //       dataLabels: {
-        //         position: 'top',
-        //       },
-        //     }
-        //   },
-        //   dataLabels: {
-        //     enabled: true,
-        //     offsetX: -6,
-        //     style: {
-        //       fontSize: '12px',
-        //       colors: ['#fff']
-        //     }
-        //   },
-        //   stroke: {
-        //     show: true,
-        //     width: 1,
-        //     colors: ['#fff']
-        //   },
-        //   tooltip: {
-        //     shared: true,
-        //     intersect: false
-        //   },
-        //   xaxis: {
-        //     categories: [2001, 2002, 2003, 2004, 2005, 2006, 2007],
-        //   },
-        //   };
-  
-        //   var chart = new ApexCharts(document.querySelector("#chart"), options);
-        //   chart.render();
-
-        import React, { Component } from 'react';
+import React, { Component } from 'react';
 import Chart from 'react-apexcharts'; // Assuming you're using ApexCharts for React
 import './chart-styles.css'
 
-
-// class LineChart extends Component {
-//   constructor(props) {
-//     console.log(props);
-//     super(props);
-//     this.state = {
-//       options: {
-//         xaxis: {
-//           labels: {
-//             style: {
-//               fontSize: '12px', 
-//               fontFamily: 'Poppins, sans-serif', 
-//             },
-//           },
-//           title: {
-//             text: "Date",
-//             offsetX: 0,
-//             offsetY: 0,
-//             style: {
-//               color: undefined,
-//               fontSize: '12px',
-//               fontFamily: 'Poppins, sans-serif',
-//               fontWeight: 400,
-//               cssClass: 'apexcharts-xaxis-title',
-//             },
-//           },
-//           type: props.xValues
-//         },
-//         yaxis: {
-//           labels: {
-//             style: {
-//               fontSize: '12px',
-//               fontFamily: 'Helvetica, Arial, sans-serif',
-//             },
-//           },
-//           title: {
-//             text: "Tea Collection In Kilo Grams",
-//             rotate: -90,
-//             offsetX: 0,
-//             offsetY: 0,
-//             style: {
-//               color: undefined,
-//               fontSize: '12px',
-//               fontFamily: 'Poppins, sans-serif',
-//               fontWeight: 400,
-//               cssClass: 'apexcharts-yaxis-title',
-//             },
-//           },
-//         },
-//       },
-//       series: [{
-//         data : props.data
-//       }],
-//     };
-//   }
-
-//   render() {
-//     return (
-//     <body>
-//       <Chart
-//         style={{margin: 'auto', backgroundColor: 'white'}}
-//         options={this.state.options}
-//         series={this.state.series}
-//         type="area"
-//         height={365}
-//       />
-//     </body>
-//     );
-//   }
-// }
-
-// export default LineChart;
-
-// class DualBarChart extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//         options: {
-//             series: [{
-//             data: [44, 55, 41, 64, 22, 43, 21]
-//             }, {
-//             data: [53, 32, 33, 52, 13, 44, 32]
-//             }],
-//             chart: {
-//             type: 'bar',
-//             height: 430
-//             },
-//             plotOptions: {
-//             bar: {
-//                 horizontal: true,
-//                 dataLabels: {
-//                 position: 'top',
-//                 },
-//             }
-//             },
-//             dataLabels: {
-//             enabled: true,
-//             offsetX: -6,
-//             style: {
-//                 fontSize: '12px',
-//                 colors: ['#fff']
-//             }
-//             },
-//             stroke: {
-//             show: true,
-//             width: 1,
-//             colors: ['#fff']
-//             },
-//             tooltip: {
-//             shared: true,
-//             intersect: false
-//             },
-//             xaxis: {
-//             categories: [2001, 2002, 2003, 2004, 2005, 2006, 2007],
-//             },
-//         }
-//         };
-//     }
-    
-//     render() {
-//         return (
-//         <Chart
-//             style={{margin: 'auto', backgroundColor: 'white', fontSize: '19px', fontFamily: 'Poppins, sans-serif'}}
-//             options={this.state.options}
-//             series={this.state.series}
-//             type="bar"
-//             width="100%"
-//             height="100%"
-//         />
-//         );
-//     }
-//     }
 
 export default class DualBarChart extends Component {
     constructor(props) {
@@ -195,7 +19,8 @@ export default class DualBarChart extends Component {
                 },
                 plotOptions: {
                     bar: {
-                        horizontal: false,
+                        horizontal: props?.horizontalx ? props?.horizontalx : false,
+                        borderRadius: 4,
                         dataLabels: {
                             position: 'top',
                         },
@@ -220,6 +45,14 @@ export default class DualBarChart extends Component {
                 },
                 xaxis: {
                     categories: props?.categories,
+                    title: {
+                        text: props?.xTitle ? props?.xTitle : 'X-Axis'
+                    },
+                },
+                yaxis: {
+                    title: {
+                        text: props?.yTitle ? props?.yTitle : 'Y-Axis'
+                    }
                 },
             },
             series: [{
@@ -238,7 +71,7 @@ export default class DualBarChart extends Component {
                 series={this.state.series}
                 type="bar"
                 width="100%"
-                height="100%"
+                height="350px"
             />
         );
     }
